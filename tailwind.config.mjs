29a27990c1943e8783fa985a4bbf46f00a27d96e
chun37 <chun.jb.37@gmail.com>
2024-05-11 17:57:02 +0900

    いい感じになった

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -66,6 +66,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        "noto-sans-jp": ["Noto Sans JP"],
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
